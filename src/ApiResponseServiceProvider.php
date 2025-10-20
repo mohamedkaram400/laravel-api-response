18d9a31ed6528dfccaa266c2165db8a193ccd231
Mohamed Karam <mohamedkaramm500@gmail.com>
2025-10-20 22:34:21 +0300

    Implement packege for api response trait

---
+++ src/ApiResponseServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MohamedKaram500\LaravelApiResponse;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiResponseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Here you could bind services if needed later
+    }
+
+    public function boot()
+    {
+        // Currently nothing to boot, but you can later add config or publishes
+    }
+}
